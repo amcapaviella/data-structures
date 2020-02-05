1aae40248d0e7ba4d8b88904a7beafa0df57fe1d
AustinCapaviella <amcapaviella@gmail.com>
2020-02-05 14:47:50 -0800

    Create Functional Pattern Stack Creation Function
    Create stack creation function using a functional pattern.
    Stacks have functionality to add items, remove items, and check size
    using the pop, push, and size functions

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,16 +3,36 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var count = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[count] = value;
+    count++;
+    return count;
   };
 
   someInstance.pop = function() {
+    var popped = storage[count - 1]
+    delete storage[count - 1]
+    if (count > 0){
+      count--;
+    } 
+    return popped;
   };
 
+
+   
   someInstance.size = function() {
+    return count;
   };
 
   return someInstance;
+
 };
+
+
+
+
+
+
