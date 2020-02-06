c7dfbe8936d9a9a2424cc4be1fbf7bc5d37fc536
AustinCapaviella <amcapaviella@gmail.com>
2020-02-05 20:32:27 -0800

    Complete stack.js with Prototypal Instantiation
    Use prototypal instantiation to create stack instances
    with push, pop and size method access

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,43 @@
+//JUSTIFICATION- create a function that creates stack instance and gives it methods using PROTOTYPAL
+
+
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  //create a stack instance using Object.create
+  var stackInstance = Object.create(stackMethods);
+
+  //create a storage object for the stack
+  stackInstance.storage = {};
+  stackInstance.count = 0;
+
+  //return the instance
+  return stackInstance;
+
+  
 };
 
-var stackMethods = {};
+var stackMethods = {
+
+  //push function
+  push: function(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    return this.storage[this.count];
+  },
+
+  //pull function
+  pop: function() {
+    var popped = this.storage[this.count - 1];
+    delete this.storage[this.count - 1];
+    if (this.count > 0) {
+      this.count--;
+    }
+    return popped;
+  },
+  
+  //size function
+  size: function() {
+    return this.count;
+  }
+};
 
 
