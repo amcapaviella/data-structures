671deef874f2cfa21b974dcc14ce1ef254e0873f
AustinCapaviella <amcapaviella@gmail.com>
2020-02-06 10:21:44 -0800

    Complete stack.js with E6S Structure

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -1,7 +1,31 @@
 class Stack {
+  
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  push(value) {
+    this.storage[this.count] = value;
+    this.count++;
+    return this.storage[this.count];
+  }
+
+  pop() {
+    var popped = this.storage[this.count - 1];
+    delete this.storage[this.count - 1];
+    if (this.count > 0) {
+      this.count--;
+    } 
+    return popped;
+  }
+
+  size() {
+    return this.count;
   }
 
 }
+
+var instanceOfStack = new Stack();
