86513dfae82d92c358c8b064d499a5fd44c6148c
AustinCapaviella <amcapaviella@gmail.com>
2020-02-06 10:20:22 -0800

    Complete stack.js with Pseudoclassical Structure

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.count = 0;
+  this.storage = {};
 };
 
+//create a prototype functions for push, pop, size
+Stack.prototype.push = function(value) {
+  this.storage[this.count] = value;
+  this.count++;
+  return this.storage[this.count];
 
+};
+
+Stack.prototype.pop = function() {
+  var popped = this.storage[this.count - 1];
+  delete this.storage[this.count - 1];
+  if (this.count > 0) {
+    this.count--;
+  } 
+  return popped;
+
+};
+
+Stack.prototype.size = function() {
+  return this.count;
+
+};
+
+var instanceOfStack = new Stack();
