234986ba59a8a2fb799181ec39a2f01bc9961187
AustinCapaviella <amcapaviella@gmail.com>
2020-02-06 10:30:59 -0800

    Complete queue.js With ES6 Structure

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,33 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.count = 0;
+  }
+
+  dequeue() {
+    var popped = this.storage['0'];
+    var position = 0;
+    for (var key in this.storage) {
+      this.storage[position] = this.storage[position + 1];
+      position++;
+    }
+    delete this.storage[this.count - 1];
+
+    if (this.count > 0) {
+      this.count--;
+    }
+    return popped;
+  }
+
+  enqueue(value) {
+    this.storage[this.count] = value;
+    this.count++;
+
+  }
+
+  size() {
+    return this.count;
   }
 
 }
