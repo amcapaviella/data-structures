e3c4342fb57ebf40e598e9247cb82275f6ffe6e8
AustinCapaviella <amcapaviella@gmail.com>
2020-02-07 11:13:35 -0800

    Complete tree.js, Add Progress on Graph

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -2,35 +2,72 @@
 
 // Instantiate a new graph
 var Graph = function() {
+	this.storage = {};
 };
 
 // Add a node to the graph, passing in the node's value.
 Graph.prototype.addNode = function(node) {
+	//add value and edges property to the node, add them to this.storage
+	//creates objet with a key of node and a value of empty array, then add it to storage 
+	this.storage[node] = [];
 };
 
 // Return a boolean value indicating if the value passed to contains is represented in the graph.
 Graph.prototype.contains = function(node) {
+	//this.storage hasOwnProperty [node]
+	return this.storage.hasOwnProperty(node);
 };
 
 // Removes a node from the graph.
 Graph.prototype.removeNode = function(node) {
+	//delete this.storage.node
+	delete this.storage[node];
+	
+	
+
 };
 
 // Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+	//look up fromNode and see if fromNodes array contains toNode's value
+	//return boolean
+	return this.storage[fromNode].includes(toNode);
+	
 };
 
 // Connects two nodes in a graph by adding an edge between them.
 Graph.prototype.addEdge = function(fromNode, toNode) {
+	//add the toNode value to fromNode array and fromNode value to toNode array
+	this.storage[fromNode].push(toNode);
+	this.storage[toNode].push(fromNode);
+
 };
 
 // Remove an edge between any two specified (by value) nodes.
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+	console.log(this.storage[fromNode], toNode)
+	//remove toNode value from fromNode array and fromNode from toNode
+	for (var i = 0; i < this.storage[fromNode].length; i++) {
+		if (this.storage[fromNode][i] === toNode) {
+			//console.log(this.storage[fromNode], this.storage[fromNode].splice(i, 1));
+			this.storage[fromNode] = this.storage[fromNode].splice(i, 1);
+		}
+	}
+	for (var i = 0; i < this.storage[toNode].length; i++) {
+		if (this.storage[fromNode][i] === fromNode) {
+			this.storage[froNode] = this.storage[toNode].splice(i, 1);
+		}
+	}
+
 };
 
 // Pass in a callback which will be executed on each node of the graph.
 Graph.prototype.forEachNode = function(cb) {
+	_.each(this.storage, cb);
+	
 };
+
+var graphInstance = new Graph();
 
 /*
  * Complexity: What is the time complexity of the above functions?
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,20 +1,57 @@
+//create a tree instance with addChild and contains functionality
+//input = value to start tree 
+//output = tree
+
+//children of each tree are stored in an array
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
+  newTree.children = []; 
+  
+  _.extend(newTree, treeMethods);
 
   return newTree;
 };
 
 var treeMethods = {};
 
+//adds a child to a tree node array
+//input is a value
+//effect = uses .children to add it to the child array
 treeMethods.addChild = function(value) {
+//childTree would be Tree(value)
+var childTree = Tree(value);
+//add childTree to this.children
+this.children.push(childTree);
+
+
 };
 
+//checks for a particular value among all of the nodes
+//input is a target value
+//output is a boolean
+//looks at first node, looks at children of that node....
 treeMethods.contains = function(target) {
+	//loop through values, check the value, if not check all the children
+	var isThere = false;
+	if(this.value === target){
+		isThere = true;
+	} else {
+		for (var i = 0; i < this.children.length; i++) {
+		isThere = isThere || this.children[i].contains(target);
+			
+		} 
+	} 
+	return isThere;
+
+	//*base case* if tree.children.item === target return true || 
+	//we run out of children return false  *base case* 
+	
 };
+
+//recursive case: if there are children and we havent found it keep going
+
+
 
 
 
